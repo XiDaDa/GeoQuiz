856fd38290f57287367c71f56ffc5dc3c40dee6d
xiexueyun <775093708@qq.com>
2015-10-30 17:21:42 +0800

    Day 2

--- src/com/android/geoquiz/QuizActivity.java
+++ src/com/android/geoquiz/QuizActivity.java
@@ -1,18 +1,114 @@
 package com.android.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
-
+	
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private Button mPreButton;
+	private Button mCheatButton;
+	private TextView mQuestionTextView;
+	private int mCurrentIndex = 0;
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans,true),
+			new TrueFalse(R.string.question_mideast,false),
+			new TrueFalse(R.string.question_africa,false),
+			new TrueFalse(R.string.question_americas,true),
+			new TrueFalse(R.string.question_asia,true),
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		if(savedInstanceState != null){
+			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+		}
+		mQuestionTextView = (TextView)findViewById(R.id.question_textview);
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		updateQuestion();
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(true);
+			}	
+		});
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(false);
+			}
+		});
+		mPreButton = (Button)findViewById(R.id.pre_button);
+		mPreButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(mCurrentIndex>=1){					
+					mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+					updateQuestion();
+				}
+			}
+		});
+		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		mCheatButton = (Button)findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent it = new Intent(QuizActivity.this,CheatActivity.class);
+				startActivity(it);
+			}
+		});
+		
 	}
-
+	
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState){
+		super.onSaveInstanceState(savedInstanceState);
+		Log.d(TAG, "saveInstanceState");
+		savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,4 +127,21 @@ public class QuizActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void updateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int messageId = 0;
+		if (userPressedTrue == answerIsTrue){
+			messageId = R.string.correct_toast;
+		}else{
+			messageId = R.string.incorrect_toast;
+		}
+		Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+	}
+	
 }
